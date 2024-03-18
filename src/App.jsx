4b6f586b84307419b68fbe337b75ca487d10dbd5
BlindDreamer <blinddreaemr@huku.rocks>
2024-03-18 22:32:37 +0200

    footer

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,14 @@ import viteLogo from "/vite.svg";
 import "./App.css";
 import Calculator from "./components/Calculator";
 import Header from "./components/Header";
+import Footer from "./components/Footer";
 
 function App() {
   return (
     <div>
       <Header />
       <Calculator />
+      <Footer />
     </div>
   );
 }
