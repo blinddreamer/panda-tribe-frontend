782d8ae8977bd99996c754accc5d745c3488b720
BlindDreamer <blinddreaemr@huku.rocks>
2024-03-24 04:34:38 +0200

    lil fixes

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,7 +2,6 @@ import DarkModeToggle from "../components/darkmode";
 import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
-import { Routes, Route, Outlet, Link } from "react-router-dom";
 
 function Header() {
   return (
@@ -10,7 +9,6 @@ function Header() {
       <Navbar expand="lg" className="bg-body-tertiary navbarh">
         <Container>
           <Navbar.Brand>eve-helper</Navbar.Brand>
-
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="me-auto">
