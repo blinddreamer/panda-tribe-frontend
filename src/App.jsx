4c16a6b521d2d23e8c8718b01cad9a6cb6980b0b
BlindDreamer <blinddreaemr@huku.rocks>
2024-04-01 18:09:22 +0300

    export navbar to component

--- src/App.jsx
+++ src/App.jsx
@@ -3,15 +3,11 @@ import "./App.css";
 import { BrowserRouter, Link, Routes, Route } from "react-router-dom";
 import CalculatorHome from "./components/CalculatorHome";
 import Home from "./components/Homepage";
-import DarkModeToggle from "./components/DarkModeToggle";
 import Footer from "./components/Footer";
-import Navbar from "react-bootstrap/Navbar";
-import Container from "react-bootstrap/Container";
-import Nav from "react-bootstrap/Nav";
-import ResponsiveAlert from "./components/ResponsiveAlert";
 import PageNotFound from "./components/PageNotFound";
 import Appraisal from "./components/Appraisal";
 import Video from "./components/Video";
+import NavbarMenu from "./components/NavbarMenu";
 
 const App = () => {
   return (
@@ -19,36 +15,8 @@ const App = () => {
       <Video />
       <BrowserRouter>
         <div id="HukuBartopolos">
-          <Navbar expand="lg" className="bg-body-tertiary navbarh">
-            <Container>
-              <Navbar.Brand>eve-helper</Navbar.Brand>
-              <Navbar.Toggle aria-controls="basic-navbar-nav" />
-              <Navbar.Collapse id="basic-navbar-nav">
-                <Nav className="me-auto">
-                  <Nav.Link as={Link} to="/">
-                    Home
-                  </Nav.Link>
-                  <Nav.Link as={Link} to="/Calculator">
-                    Calculator
-                  </Nav.Link>
-                  <Nav.Link as={Link} to="/Appraisal">
-                    Appraisal
-                  </Nav.Link>
-                </Nav>
-                <Nav>
-                  <DarkModeToggle />
-                </Nav>
-              </Navbar.Collapse>
-            </Container>
-          </Navbar>
-          <p></p>
+          <NavbarMenu />
           {/*<main id="swup" className="transition-main">*/}
-          <div>
-            <ResponsiveAlert
-              breakpointWidth={1450}
-              message="Optimizing for desktop first, screen resolution too low!"
-            />
-          </div>
           <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/Calculator" element={<CalculatorHome />} />
---
+++ src/components/NavbarMenu.jsx
@@ -0,0 +1,48 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import DarkModeToggle from "./DarkModeToggle";
+import Navbar from "react-bootstrap/Navbar";
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import ResponsiveAlert from "./ResponsiveAlert";
+import Video from "./Video";
+
+function NavbarMenu() {
+  return (
+    <>
+      <Video />
+      <Navbar expand="lg" className="bg-body-tertiary navbarh">
+        <Container>
+          <Navbar.Brand>eve-helper</Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="me-auto">
+              <Nav.Link as={Link} to="/">
+                Home
+              </Nav.Link>
+              <Nav.Link as={Link} to="/Calculator">
+                Calculator
+              </Nav.Link>
+              <Nav.Link as={Link} to="/Appraisal">
+                Appraisal
+              </Nav.Link>
+            </Nav>
+            <Nav>
+              <DarkModeToggle />
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+      <p></p>
+      <div>
+        <ResponsiveAlert
+          breakpointWidth={1450}
+          message="Optimizing for desktop first, screen resolution too low!"
+        />
+      </div>
+      <p></p>
+    </>
+  );
+}
+
+export default NavbarMenu;
